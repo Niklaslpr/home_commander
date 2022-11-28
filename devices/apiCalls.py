25d98a197c57e431676eeedacd00c00560e90a06
brezel42 <brezel42@proton.me>
2022-11-28 14:41:58 +0100

    new URL

--- devices/apiCalls.py
+++ devices/apiCalls.py
@@ -4,14 +4,14 @@ import time
 
 def putstate1(state):
     data = '{"on":' + state + '}'
-    p = requests.put('http://172.20.10.4/api/973FC5C763/lights/4/state', data = data)
+    p = requests.put('http://172.20.10.4:8080/api/973FC5C763/lights/4/state', data = data)
     print(p.status_code)
     print(p.content)
 
 
 def putstate(state, lightID):
     data = '{"on":' + state + '}'
-    url = 'http://172.20.10.4/api/973FC5C763/lights/' + lightID + '/state'
+    url = 'http://172.20.10.4:8080/api/973FC5C763/lights/' + lightID + '/state'
     p = requests.put(url, data=data)
     print(p.status_code)
     print(p.content)
@@ -20,7 +20,7 @@ def putstate(state, lightID):
 def putbri(bri, id):
     print(bri)
     data = '{"bri":' + bri + '}'
-    url = 'http://172.20.10.4/api/973FC5C763/lights/' + id + '/state'
+    url = 'http://172.20.10.4:8080/api/973FC5C763/lights/' + id + '/state'
     p = requests.put(url, data=data)
     print(p.status_code)
     print(p.content)
@@ -31,7 +31,7 @@ def puthue(hue, sat, id):
     print(sat)
     print(id)
     data = '{"hue":' + hue + ', "sat":' + sat + '}'
-    url = 'http://172.20.10.4/api/973FC5C763/lights/' + id + '/state'
+    url = 'http://172.20.10.4:8080/api/973FC5C763/lights/' + id + '/state'
     p = requests.put(url, data=data)
     print(p.status_code)
     print(p.content)
@@ -39,7 +39,7 @@ def puthue(hue, sat, id):
 def startscan():
 
     # Aktuelle Liste der Geräte ausgeben
-    url = 'http://172.20.10.4/api/973FC5C763/lights'
+    url = 'http://172.20.10.4:8080/api/973FC5C763/lights'
     p = requests.get(url)
     print(p.status_code)
     dict = p.json()
@@ -51,14 +51,14 @@ def startscan():
             print('ID: ' + key + ', Value: ' + value['manufacturername'] + ', ' + value['name'])
 
     # Suche starten
-    url = 'http://172.20.10.4/api/973FC5C763/sensors'        #findet Neue Lampe
+    url = 'http://172.20.10.4:8080/api/973FC5C763/sensors'        #findet Neue Lampe
     p = requests.post(url)
     print(p.status_code)
     print(p.content)
 
     # Nach 300 Sekunden neue Liste mit der alten vergleiche und neue Geräte ausgeben
     time.sleep(180)
-    url = 'http://172.20.10.4/api/973FC5C763/lights'
+    url = 'http://172.20.10.4:8080/api/973FC5C763/lights'
     p = requests.get(url)
     print(p.status_code)
     dict2 = p.json()
