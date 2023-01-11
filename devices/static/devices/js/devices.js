f04d876bcb3b52371fe7ff2bd463707946716a3b
Niklaslpr <niklas.loerper@fom-net.de>
2023-01-11 14:33:09 +0100

    Icon-Picker for devices

--- devices/static/devices/js/devices.js
+++ devices/static/devices/js/devices.js
@@ -9,6 +9,8 @@ let deviceToFav
 let deviceToFavLabel
 let deviceToFavText
 let deleteDeviceButton
+let selectedIcon
+let icons
 
 $(document).ready(() => {
     deviceControlColorPicker = new iro.ColorPicker('#device-control-modal-colorpicker', {
@@ -340,4 +342,19 @@ function saveDeviceDataToLocalStorage(deviceId) {
     } else {
         return null;
     }
+}
+
+function getIconId(IconId){
+    icons = ["lamp-fill.svg", "lamp.svg", "lightbulb.svg", "lightbulb-fill.svg", "plug.svg", "plug-fill.svg", "brezel.png", "plugin.svg", "robot.svg"];
+    for (const tmp in icons){
+        document.getElementById(icons[tmp]).style.backgroundColor = "transparent";
+    }
+    selectedIcon = IconId;
+    document.getElementById(selectedIcon).style.backgroundColor = "var(--tertiary-color)";
+}
+
+function saveIcon(){
+    document.getElementById('modal-body-edit').hidden = true;
+    document.getElementById('modal-body-normal').hidden = false;
+    console.log(selectedIcon);
 }
