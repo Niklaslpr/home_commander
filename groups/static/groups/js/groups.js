f3f6a81d0335229f6c644839685304f107281887
Niklaslpr <niklas.loerper@fom-net.de>
2023-01-11 15:04:59 +0100

    Icon-Picker for Groups

--- groups/static/groups/js/groups.js
+++ groups/static/groups/js/groups.js
@@ -5,6 +5,8 @@ let groupControlModalLabel;
 let groupControlModalSwitch;
 let groupControlModalBrightnessSlider;
 let groupControlModalBrightnessDisplay;
+let selectedIcon
+let icons
 
 $(document).ready(() => {
     groupControlColorPicker = new iro.ColorPicker('#group-control-modal-colorpicker', {
@@ -143,4 +145,19 @@ function saveGroupDataToLocalStorage(groupId) {
     } else {
         return null;
     }
+}
+
+function getIconId(IconId){
+    icons = ["lamp-fill.svg", "lamp.svg", "lightbulb.svg", "lightbulb-fill.svg", "plug.svg", "plug-fill.svg", "brezel.png", "plugin.svg", "robot.svg"];
+    for (const tmp in icons){
+        document.getElementById(icons[tmp]).style.backgroundColor = "transparent";
+    }
+    selectedIcon = IconId;
+    document.getElementById(selectedIcon).style.backgroundColor = "var(--tertiary-color)";
+}
+
+function saveIcon(){
+    document.getElementById('modal-body-edit').hidden = true;
+    document.getElementById('modal-body-normal').hidden = false;
+    console.log(selectedIcon);
 }
