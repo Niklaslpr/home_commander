92d7ae7ac812cce00a5bce5a60c05e6c5aced3e1
brezel42 <brezel42@proton.me>
2023-01-20 15:39:29 +0100

    zusammen

--- scenes/static/scenes/js/build.js
+++ scenes/static/scenes/js/build.js
@@ -15,29 +15,32 @@ function loadScenes() {
             console.info(data);
 
             let scenesJson = {};
-            for (let entry of data.scenes) {
-                scenesJson[entry['id']] = entry;
+            for (let group in data.scenes) {
+                for (let entry of data.scenes[group]) {
+                    scenesJson[entry['id']] = entry;
+                    scenesJson[entry['id']]['group_id'] = group;
 
-                $.ajax({
-                    url: './kit/scene-tile',
-                    type: 'get',
-                    data: {
-                        "csrfmiddlewaretoken": getCookie('csrftoken'),
-                        "scene-id": entry['id'].toString(),
-                        "scene-name": entry['name'].toString(),
-                        "scene-icon": entry['icon'].toString(),
-                    },
-                    headers: {
-                        'Content-type': 'application/json', 'Accept': 'text/plain',
-                        'X-CSRFToken': getCookie('csrftoken')
-                    },
-                    dataType: 'json',
-                    mode: 'same-origin'
-                }).always((data) => {
-                    if (data.readyState === 4 && data.status === 200) {
-                        document.getElementById('scene-list').insertAdjacentHTML('afterbegin', data.responseText.toString());
-                    }
-                });
+                    $.ajax({
+                        url: './kit/scene-tile',
+                        type: 'get',
+                        data: {
+                            "csrfmiddlewaretoken": getCookie('csrftoken'),
+                            "scene-id": entry['id'].toString(),
+                            "scene-name": entry['name'].toString(),
+                            "scene-icon": entry['icon'].toString(),
+                        },
+                        headers: {
+                            'Content-type': 'application/json', 'Accept': 'text/plain',
+                            'X-CSRFToken': getCookie('csrftoken')
+                        },
+                        dataType: 'json',
+                        mode: 'same-origin'
+                    }).always((data) => {
+                        if (data.readyState === 4 && data.status === 200) {
+                            document.getElementById('scene-list').insertAdjacentHTML('afterbegin', data.responseText.toString());
+                        }
+                    });
+                }
             }
 
             window.localStorage.setItem('scenes', JSON.stringify(scenesJson));
